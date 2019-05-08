60e5cb1692e5e42b5505c639cd1f02558b987c8a
Rachael Blake <rblake@rstudio03.research.sesync.org>
2019-05-08 17:28:26 -0400

    revised text color and size; other styling

--- ui.R
+++ ui.R
@@ -6,8 +6,8 @@
 library(shiny) ; library(leaflet) 
 
 # Set up your panels for the UI
-tabpan_peru <- tabPanel(title = "Peru",
-                        div(class = "outer", tags$head(includeCSS("styles.css"))),
+tabpan_peru <- tabPanel(title = p("Peru Map", style = "font-size: 18px;"),
+                        div(class = "outer", tags$head(includeCSS("styles.css")),
                         leafletOutput(outputId = "map_peru", width = "100%", height = "100%") , # 
                         absolutePanel(top = 240, right = 5, left = "auto", draggable = TRUE, fixed = TRUE,
                                       width = 330, height = "auto",
@@ -25,6 +25,7 @@ tabpan_peru <- tabPanel(title = "Peru",
                                                          selected = c(3)
                                                          )
                                       )
+                            )
                         )
 
 # tabpan_globe <- tabPanel(title = "Global",
@@ -40,14 +41,14 @@ tabpan_peru <- tabPanel(title = "Peru",
 #                                        )
 #                          )
 
-tabpan_info <- tabPanel("More Information",
-                        h4("About this application"),
+tabpan_info <- tabPanel(title = p("More Information", style = "font-size: 18px;"),
+                        h3("About this application"),
                         HTML('<p>This is more information
                              <br/>
                              about your app here.'),
                         br(),
                         br(),
-                        h4("How to cite this page, and source code"),
+                        h3("How to cite this page, and source code"),
                         HTML('<p> YOUR CITATION TO YOUR PAPER HERE
                              <br/>
                              Code is available from: XXXXXX')
@@ -57,7 +58,7 @@ tabpan_info <- tabPanel("More Information",
 
 
 ### UI
-ui <- htmlTemplate("template.html", content = navbarPage(title = "Oil Palm Deforestation Mapping",
+ui <- htmlTemplate("template.html", content = navbarPage(title = strong(""),
                                                          tabpan_peru, 
                                                          # tabpan_globe, 
                                                          tabpan_info,
